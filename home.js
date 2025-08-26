b43bcb49ebd67e343ae7645f8b6eeb271d3acfd2
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-08-26 21:15:10 +0600

    Validation added

--- home.js
+++ home.js
@@ -1,6 +1,14 @@
 document.getElementById('add-money-btn').addEventListener('click', function(e) {
     e.preventDefault();
     const currentBalance = parseInt(document.getElementById('current-balance').innerText);
+    const bank = document.getElementById('bank').value;
+    const accountNumber = document.getElementById('account-number').value;
     const addMoney = parseInt(document.getElementById('add-amount').value);
+    const pin = parseInt(document.getElementById('pin-number').value);
+    if (bank === 'Select a back' || accountNumber.length != '01643496398' || addMoney < 0 || pin !== 2244) {
+        alert('invalid credentials');
+        return;        
+    }
+    
     document.getElementById('current-balance').innerText = currentBalance + addMoney;
 })
